0ee6e902a954ca9d062cb403fa3b5b65561eab51
Matt Martindale <martindalematt18@yahoo.com>
2020-08-04 20:04:40 -0600

    Made the objc model controller available in Swift

--- Contacts-Swift-Hybrid/Contacts-Hybrid/ContactsTableViewController.swift
+++ Contacts-Swift-Hybrid/Contacts-Hybrid/ContactsTableViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 
 class ContactsTableViewController: UITableViewController {
+    
+    let contactsController = ContactController()
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
